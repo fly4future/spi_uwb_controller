7fd33db4a9751925adbcb8633b6f4e4e2e68b2fe
Viktor Walter <viktor.walter@disroot.org>
2025-02-04 17:51:29 +0100

    Making the reload command use intput to set specified ID. This is then also called from launcher where the output number is input into the uwb_mac_addr parameter.

--- driver/reload.sh
+++ driver/reload.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+if [ -z "$1" ]; then
+	input=-1
+	ID=1234
+else
+	input=${1}
+	ID=`printf '%x\n' ${1}`
+fi
+
 cd "$(dirname "$0")"
 sudo rmmod dw3000_drv
 sudo insmod ./dw3000-drv.ko
@@ -9,11 +17,12 @@ fi
 sleep 1
 sudo ip link set wpan0 down &&
 sudo iwpan dev wpan0 set pan_id "0xabcd" && 
-sudo iwpan dev wpan0 set short_addr "0x1234" &&
+sudo iwpan dev wpan0 set short_addr "0x${ID}" &&
 sudo ip link set wpan0 up &&
+
+echo $input
 return 0
 
-return 1
 
 #sudo ip link set wpan1 down
 #sudo iwpan dev wpan1 set pan_id "0xabcd"
